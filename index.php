62786ebd175da3f224bdaf9412a19492d3067ced
Vo Tan Khang <khangvtps34518@fpt.edu.vn>
2024-05-30 18:28:00 +0700

    create index.hml

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/9fc185c8a4.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="rating-review">
+            <div class="tri table flex">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="rnb rvl">
+                                    <h3>1.5/5.0</h3>
+                                </div>
+                                <div class="pdt-rate">
+                                    <div class="pro-rating">
+                                        <div class="clearfix">
+                                            <div class="clearfix rating mart8">
+                                            </div>
+                                            <div class="rating-stars">
+                                                <div class="grey-stars"></div>
+                                                <div class="filled-stars" style="width:60%"></div>
+                                            </div>
+                                            <div></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="rnrn">
+                                    <p class="rars">
+                                        No Reviews
+                                    </p>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="rpb">
+                                    <div class="rnpb">
+                                        <label for="">5<i class="fa fa-star"></i></label>
+                                        <div class="ropb">
+                                            <div class="ripb" style="width: 20%">
+
+                                            </div>
+                                            <div class="label">
+                                              (1)  
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+<script src="script.js"></script>
